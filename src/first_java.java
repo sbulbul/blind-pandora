a4fd39596ef18358fd22b328a7564ef2c9c9d4ec
Salim <salimm.bulbul@gmail.com>
2022-03-26 15:58:03 -0400

    third one

--- src/first_java.java
+++ src/first_java.java
@@ -1,5 +1,7 @@
 public class first_java {
     public static void main(String[] args) {
         System.out.println("Yeyyy github");
+        System.out.println("Adding a new code");
+        System.out.println("This is another code");
     }
 }
